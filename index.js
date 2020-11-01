44ec2727a12930ae45b6503bed04c4182012c9de
ina <bbangina@gmail.com>
2020-11-01 23:58:19 +0900

    post, comment 필드 추가

--- index.js
+++ index.js
@@ -3,26 +3,73 @@ const { graphqlHTTP } = require('express-graphql');
 const { graphql, buildSchema } = require('graphql');
 //스키마 생성(데이터 타입, 엔드포인트등 정의)
 //must be "Query", not any other word
+//Int! : ! means required
+//post(id)==> 1건 조회
+//posts --> List 
+//Post에 Comment 필드를 가져와서 추가
 const schema = buildSchema(`
   type Query {
-    getHello: Int
-    getPost: Post
+    post(id: Int!): Post
+    posts: [Post]
   }
   type Post {
       id: Int
       title: String
+      comments: [Comment]
+  }
+  type Comment{
+      text: String
+      user: String
   }
 `);
 
+const posts = [
+    {
+        id:1,
+        title: 'GraphQL',
+        comments:[
+            {
+            text:"what is up",
+            user:"Ina"
+        }
+        ]
+    },
+    {
+        id:2,
+        title: 'GraphQL with Express',
+        comments:[
+            {
+            text:"what is up",
+            user:"Ina"
+            }
+        ]
+    },
+    {
+        id:3,
+        title: 'So much funnnn',
+        comments:[
+            {
+            text:"what is up",
+            user:"Ina"
+            }
+        ]
+    },
+]
+
 //Resolver(MVC패턴의 Controller 비슷)
 const root = { 
-    getHello: () => 5, 
-    getPost: () => ({
-        id: 1,
-        title:"hello world"
-    })
+    // post: ({id}) => {
+    //     return posts[id];
+    //     }
+    post: ({id}) => {
+        return posts.find(post => post.id===id);
+        },
+    posts: ()=> {
+        return posts;
+    }
+    
 };
-
+ 
 const app = express();
 
 app.use(
